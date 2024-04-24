02d45d1b39cfdb6d35c20980709e3707d5ec3e15
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-04-24 21:00:42 +0700

    preparation du cours

---
+++ Docs/E01/playground/index.js
@@ -0,0 +1,6 @@
+const fruits = [
+  { id: 1, name: 'Kiwi', unitPrice: 1, quantity: 6 },
+  { id: 2, name: 'Pomme', unitPrice: 0.8, quantity: 2 },
+  { id: 3, name: 'Raisin', unitPrice: 2.5, quantity: 1 },
+  { id: 4, name: 'Fraise', unitPrice: 3.5, quantity: 1 },
+];
