16f970076a540b6c19bb92b621f1dd10184d070d
yin-lo <yinlo@hotmail.fr>
2024-04-25 15:41:50 +0200

    add open/close modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,68 @@
+// un objet qui contient des fonctions
+const app = {
 
-// on objet qui contient des fonctions
-var app = {
+  addListenerToActions() {
+    // ouvrir une modale
+    const buttonAddModal = document.getElementById('addListButton');
+    buttonAddModal.addEventListener('click', (app.showAddListModal));
+    
 
-  // fonction d'initialisation, lancée au chargement de la page
-  init: function () {
-    console.log('app.init !');
-  }
+    // fermer une modale
+    const buttonCloseModal = document.querySelectorAll('.close');
+    buttonCloseModal.forEach((button) => {
+      button.addEventListener('click',
+        (app.hideModals));
+    });
 
+    // récupérer formulaire ajout d'une liste
+    const form = document.querySelector('#addListForm');
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      app.handleAddListFrom(event.target);
+    })
+  },
+
+  // ouvrir une modale
+  showAddListModal() {
+    const addModal = document.getElementById('addListModal');
+    addModal.classList.add('is-active');
+  },
+
+  // fermer tous les boutons close
+  hideModals() {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach((modal) => {
+      modal.classList.remove('is-active');
+    });
+  },
+
+  // récupérer formulaire ajout d'une liste
+  newData: [],
+
+  handleAddListFrom(event) {
+    const formData = new FormData(event);
+ 
+    formData.forEach((value, key) => {
+      let theList = {}
+      theList[key] = value;
+      app.newData.push(theList);
+    })
+    console.log(app.newData);
+  },
+
+  makeListInDOM() {
+    const listTemplate = document.getElementById('list-template');
+    const listClone = document.importNode(listTemplate.content, true);
+    // console.log(listClone);
+    listClone.querySelector('[slot="list-name"]').textContent = list.name;
+  },
+
+    // fonction d'initialisation, lancée au chargement de la page
+    init() {
+      app.addListenerToActions();
+      app.makeListInDOM();
+    }
 };
 
-
 // on accroche un écouteur d'évènement sur le document : quand le chargement est terminé, on lance app.init
-document.addEventListener('DOMContentLoaded', app.init );
+document.addEventListener('DOMContentLoaded', app.init);
