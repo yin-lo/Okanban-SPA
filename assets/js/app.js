b7b83ea650cef620e67a99412e6aefb07694108e
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-04-26 15:00:03 +0700

    valider le formulaire

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,25 +6,40 @@ const app = {
     app.addListenerToActions();
   },
   addListenerToActions: function () {
+    //* click ouvrir list modal
     const addListButton = document.getElementById('addListButton');
     addListButton.addEventListener('click', app.showAddListModal);
-
+    //* click fermer modal
     const buttonsCloseModals = document.querySelectorAll('.modal .close');
     buttonsCloseModals.forEach((elementButton) => {
       elementButton.addEventListener('click', app.hideModals);
     });
+    //* submit form ajouter liste
+    const formAddList = document.querySelector('#addListModal form');
+    formAddList.addEventListener('submit', app.handleAddListForm);
   },
   showAddListModal: function () {
     const listModal = document.getElementById('addListModal');
     listModal.classList.add('is-active');
   },
-  hideModals: function (event) {
+  hideModals: function () {
     //* closest permet de récupérer le plus element parent qui match .modal
     // const modal = event.target.closest('.modal');
     const modals = document.querySelectorAll('.modal');
     modals.forEach((modal) => {
       modal.classList.remove('is-active');
     });
+  },
+  handleAddListForm: function (event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    // console.log(formData.get('title'));
+    //* astuce
+    console.log(JSON.stringify(Object.fromEntries(formData)));
+    //* ferme la modal
+    app.hideModals();
+    //* reset du formulaire
+    event.target.reset();
   },
 };
 
