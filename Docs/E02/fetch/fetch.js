c6f18ca6cb38d478fb569cd0ce9cb3099ddaa4ae
ghislaingirardeau <g.girardeaupro@sfr.fr>
2024-04-27 15:30:24 +0700

    comment

--- Docs/E02/fetch/fetch.js
+++ Docs/E02/fetch/fetch.js
@@ -4,15 +4,20 @@
 
 async function getDatas() {
   try {
-    const response = await fetch('http://localhost:3000/lists');
+    const response = await fetch('http://localhost:3000/lists/1');
     const json = await response.json();
+    //* si la liste n'existe pas lists/155 => tout est ok mais renvoie json null => return json => if json(null) ... else alert
+    //* si la route n'est pas bonne 404 => renvoie le json dans l'erreur => return null => if null => else alert
+    //* si trouve la liste => renvoie le json => if json ...
+    console.log(response);
+
     if (!response.ok) {
       throw json;
     }
-    console.log('tout est ok');
     console.log(json);
+    console.log('tout est ok');
   } catch (error) {
-    alert('erreur fetch');
+    console.log('erreur fetch');
     console.log(error);
   }
 }
@@ -34,7 +39,7 @@ async function postData(event) {
     const response = await fetch('http://localhost:3000/lists', {
       method: 'POST',
       //? Methode 1 avec formData
-    //   body: formData, // multipart/form-data
+      //   body: formData, // multipart/form-data
       //? Methode 2 avec json
       body: JSON.stringify(Object.fromEntries(formData)),
       headers: {
